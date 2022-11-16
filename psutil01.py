2535fa42cb63283ae8aabfe3ef0c87138195f2a7
sirisha3366 <117846996+sirisha3366@users.noreply.github.com>
2022-11-16 15:51:35 +0530

    Add files via upload

---
+++ psutil01.py
@@ -0,0 +1,16 @@
+import psutil
+#result01 = psutil.cpu_times()
+
+#result02 = psutil.cpu_stats()
+
+#result03 = psutil.cpu_freq()
+
+#result04 = psutil.disk_partitions()
+
+result05 = psutil.net_io_counters(pernic=True)
+
+#print (result01)
+#print (result02)
+#print (result03)
+#print (result04)
+print (result05)
